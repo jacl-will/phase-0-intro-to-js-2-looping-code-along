07c5327c9fe2c6b47ad3d0fca1f8c6b849c5897e
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-01-02 21:19:17 -0500

    commit tree efa2763ce45af923b1e4869486f210b7c1b19ac0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = ["Becki", "Cody", "Fred"];
+//const event = ["Christmas"];
+function writeCards(name, event) {
+    let messages = [];
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+}
+return messages;
+}
+//writeCards(names);
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
